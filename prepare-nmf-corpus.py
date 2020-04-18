ad89eddd7c9ebd7d9a3814ca4946be4ab0787e0a
Chia-Hsuan Chang <sham82503@gmail.com>
2020-04-18 22:34:40 +0800

    add rolling update mode for nmf topic detection

--- prepare-nmf-corpus.py
+++ prepare-nmf-corpus.py
@@ -11,32 +11,40 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--collection', type=str, nargs="+", required=True, dest="collections",
                         help='give a list of collection name')
     parser.add_argument('-f', '--fileTag', type=str, required=True, dest="fileTag",
-                        help='fileTag as prefix for all exported files')                    
+                        help='fileTag as prefix for all exported files') 
+    parser.add_argument('-pd', '--preDictTag', type=str, dest="preDictTag",
+                        help='fileTag as prefix for all exported files')                   
     args = parser.parse_args()
 
     fileTag = args.fileTag
     collections = args.collections
+    preDictTag = args.preDictTag
+    
     query = {"tokens":{"$exists": True}}
     corpora_path = "./corpora/"
     model_path = "./models/"
 
-    #### Step 1, build dictionary object ####
-    print("Start to build dictionary object.")
     conn = MongoConnector("./config.ini")
-    dct = Dictionary()
-    # use Timer to print elapsed time
-    with Timer():
-        for each_collection in collections:
-            print("Reading the corpus from {}".format(each_collection))
-            conn.get_collection_cursor(each_collection)
-            for doc in conn.data_streaming_from_collection(query=query):
-                token_f = [x for x in doc["tokens"] if len(x) > 1]
-                dct.add_documents([token_f])
-    
-    print("Original size of vocabs: {}".format(len(dct)))
-    # control the vocabulary
-    dct.filter_extremes(no_below=40, no_above=0.5, keep_n=len(dct), keep_tokens=None)
-    print("Truncated size of vocabs: {}".format(len(dct)))
+
+    #### Step 1, build dictionary object ####
+    if preDictTag == None:
+        print("Start to build dictionary object.")
+        dct = Dictionary()
+        # use Timer to print elapsed time
+        with Timer():
+            for each_collection in collections:
+                print("Reading the corpus from {}".format(each_collection))
+                conn.get_collection_cursor(each_collection)
+                for doc in conn.data_streaming_from_collection(query=query):
+                    token_f = [x for x in doc["tokens"] if len(x) > 1]
+                    dct.add_documents([token_f])
+        
+        print("Original size of vocabs: {}".format(len(dct)))
+        # control the vocabulary
+        dct.filter_extremes(no_below=40, no_above=0.5, keep_n=len(dct), keep_tokens=None)
+        print("Truncated size of vocabs: {}".format(len(dct)))
+    elif preDictTag != None:
+        dct = Dictionary.load('{}{}.dict'.format(corpora_path,preDictTag))
 
     #### Step 2, apply Tf-IDF representation ####
     bow_corpus = []
@@ -61,6 +69,7 @@ if __name__ == "__main__":
     tfidf_corpus = tfidf_model[bow_corpus]
 
     #### Step 3, export model ####
-    dct.save('{}{}.dict'.format(corpora_path,fileTag))
+    if preDictTag == None:
+        dct.save('{}{}.dict'.format(corpora_path,fileTag))
     MmCorpus.serialize('{}{}-tf-idf.mm'.format(corpora_path,fileTag), tfidf_corpus)
     tfidf_model.save('{}{}-tf-idf.model'.format(model_path,fileTag))
--- train-nmf-model.py
+++ train-nmf-model.py
@@ -12,21 +12,36 @@ if __name__ == "__main__":
     parser.add_argument('-n', '--num_topics', type=int, required=True, dest="num_topics",
                         help='num of topics we want to infer from NMF model')
     parser.add_argument('-f', '--fileTag', type=str, required=True, dest="fileTag",
-                        help='fileTag as prefix for all exported files')                    
+                        help='fileTag as prefix for all exported files')
+    parser.add_argument('-pd', '--preDictTag', type=str, dest="preDictTag",
+                        help='fileTag as prefix for all exported files') 
+    parser.add_argument('-pf', '--preFileTag', type=str, dest="preFileTag",
+                        help='preFileTag used to select previous nmf model')                 
     args = parser.parse_args()
 
     num_topics = args.num_topics
     fileTag = args.fileTag
+    preFileTag = args.preFileTag
+    preDictTag = args.preDictTag
     corpora_path = "./corpora/"
     model_path = "./models/"
+    model_suffix = "-{}topics".format(num_topics)
 
     #### Step 1, Load Corpus ####
-    dct = Dictionary.load('{}{}.dict'.format(corpora_path,fileTag))
+    if (preFileTag == None) and (preDictTag == None):
+        dct = Dictionary.load('{}{}.dict'.format(corpora_path,fileTag))
+    # for rolling way first built model
+    elif (preFileTag == None) and (preDictTag != None):
+        dct = Dictionary.load('{}{}.dict'.format(corpora_path,preDictTag))
+
     tfidf_corpus = MmCorpus('{}{}-tf-idf.mm'.format(corpora_path,fileTag))
 
     #### Step 2, train NMF to extract topic patterns ####
-    nmf = Nmf(tfidf_corpus, id2word=dct, num_topics=num_topics)
+    if preFileTag == None:
+        nmf = Nmf(tfidf_corpus, id2word=dct, num_topics=num_topics)
+    elif preFileTag != None:
+        nmf = Nmf.load("{}{}{}.model".format(model_path,preFileTag,model_suffix))
+        nmf.update(tfidf_corpus)
 
     #### Step 3, export model ####
-    model_suffix = "-{}topics".format(num_topics)
     nmf.save("{}{}{}.model".format(model_path,fileTag,model_suffix))
