786f97c85b16f30ea1f60f701cad36331ac73a5a
Chia-Hsuan Chang <sham82503@gmail.com>
2020-04-27 01:54:48 +0800

    use bash for applying nmf by moving way

---
+++ bash-export-nmf-model-summary.sh
@@ -0,0 +1,7 @@
+python export-nmf-model-summary.py -n 25 -f W1-March-Tweets
+python export-nmf-model-summary.py -n 25 -f W1W2-March-Tweets-Moving
+python export-nmf-model-summary.py -n 25 -f W2-March-Tweets
+python export-nmf-model-summary.py -n 25 -f W2W3-March-Tweets-Moving
+python export-nmf-model-summary.py -n 25 -f W3-March-Tweets
+python export-nmf-model-summary.py -n 25 -f W3W4-March-Tweets-Moving
+python export-nmf-model-summary.py -n 25 -f W4-March-Tweets
---
+++ bash-train-nmf-model.sh
@@ -0,0 +1,7 @@
+python train-nmf-model.py -n 25 -f W1-March-Tweets
+python train-nmf-model.py -n 25 -f W1W2-March-Tweets-Moving
+python train-nmf-model.py -n 25 -f W2-March-Tweets
+python train-nmf-model.py -n 25 -f W2W3-March-Tweets-Moving
+python train-nmf-model.py -n 25 -f W3-March-Tweets
+python train-nmf-model.py -n 25 -f W3W4-March-Tweets-Moving
+python train-nmf-model.py -n 25 -f W4-March-Tweets
