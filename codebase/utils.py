f9bb235f36c736ac8b3ce4615d9d5e463d756840
Chia-Hsuan Chang <sham82503@gmail.com>
2020-07-29 03:28:15 +0800

    estimate hashtag importance for each week

--- codebase/utils.py
+++ codebase/utils.py
@@ -1,7 +1,8 @@
 import time
 import os.path
 import configparser
-from collections import namedtuple
+from collections import namedtuple, defaultdict
+import math
 
 from pymongo import MongoClient
 
@@ -113,3 +114,56 @@ class MultipleTweetCorporaBOWStream(object):
                 bow_per_doc = self.dictionary.doc2bow(a_tweet.tokens_str.split(","))
                 if len(bow_per_doc) > 4:
                     yield bow_per_doc
+
+def build_freq_table(docs):
+    """
+    Input
+        docs (enumerator), object from TweetRawCorpusStream class
+    
+    Return
+        term_freq_table (dict), key=term, value=term_count
+        document_freq_table (dict), key=term, value=#documents of term appearance
+        i+1 (int), number of documents
+    """
+    term_freq_table = defaultdict(int)
+    document_freq_table = defaultdict(int)
+    # loop for each doc
+    for i, a_tweet in docs:
+        # check whether a term has shown in a doc or not
+        term_appear = defaultdict(int)
+        doc = a_tweet.tokens_str.split(",")
+        for term in doc:
+            
+            # if term is not hashtag then continue next term
+            if not term.startswith("#"):
+                continue
+            
+            # increase 1 for term_freq_table
+            term_freq_table[term] +=1
+            
+            # increase 1 for document_freq_table when a term first appears in a doc
+            if term_appear[term] == 0:
+                term_appear[term] +=1
+                document_freq_table[term] +=1
+    
+    return term_freq_table, document_freq_table, i+1
+
+def calculate_tf_idf_table(term_freq_table, document_freq_table, N):
+    """
+    Input
+        term_freq_table (dict), key=term, value=term_count
+        document_freq_table (dict), key=term, value=#documents of term appearance
+        N (int), number of documents
+    Retrun
+        tf_idf_table (dict), key=term, value=tf-idf
+    """
+    tf_idf_table = {}
+    all_tf = sum(term_freq_table.values())
+    for term, tf in term_freq_table.items():
+        df = document_freq_table[term]
+        idf = math.log10(N / float(df))
+        tf_idf_table[term] = (tf/all_tf) * idf
+    return tf_idf_table
+
+def return_top_N_dict_by_value(table, N):
+    return dict(sorted(table.items(), key=lambda x: x[1], reverse=True)[:N])
